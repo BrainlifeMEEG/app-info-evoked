2562f50d69f41b313d3172bc0df27f7af4d8b611
Kami Salibayeva <61473793+KSalibay@users.noreply.github.com>
2024-02-23 00:39:46 -0500

    Added the code

---
+++ main.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2020 brainlife.io
+#
+# This file is a MNE python-based brainlife.io App
+#
+# Author: Kami Salibayeva
+# Indiana University
+
+# set up environment
+import os
+import json
+import mne
+
+# Current path
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+# Populate mne_config.py file with brainlife config.json
+with open(__location__+'/config.json') as config_json:
+    config = json.load(config_json)
+
+fname = config['epo']
+
+# Read the raw data and info
+evo = mne.read_evokeds(fname)
+info = evo.info
+
+#Save the info into a info.txt file
+with open(os.path.join('out_dir','info.txt'), 'w') as f:
+    f.write(str(info))
